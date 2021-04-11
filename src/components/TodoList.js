4f8be1e86a5a89da42688871eaf02241c9038c94
Baki <sat.sust@gmail.com>
2021-04-11 23:28:12 +0200

    todo list component added

---
+++ src/components/TodoList.js
@@ -0,0 +1,13 @@
+function TodoList() {
+    const todos = [
+        { id: 1, text: "Wash dishes", done: false },
+        { id: 2, text: "Do laundry", done: false },
+        { id: 3, text: "Take shower", done: false }
+      ];
+    
+    return (
+        <p>To do list</p>
+    );
+}
+
+export default TodoList
