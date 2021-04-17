b0f0465994e3983215bc40a76456bdbb27429a91
Baki <sat.sust@gmail.com>
2021-04-17 18:17:32 +0200

    components converted to styled components

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,40 +1,92 @@
 
-import classes from './AddTodo.module.css';
-import {useRef} from 'react';
+import styled from 'styled-components';
+import {useState,useRef} from 'react';
+
+const AddTodoDiv = styled.div`
+  background-color: #C0B9DD;
+  width:40%;
+  align-items: center;
+  
+  & form{
+    width:60%;
+    margin:auto;
+    padding: 20px 10px 10px 20px;
+    display:flex;
+    flex-flow:column;
+    justify-content:center;
+  }
+
+  & input{
+    background-color: ${props => (props.invalid ? '#EBB9DF' : 'white')};
+    border: 2px solid ${props => (props.invalid ? '#df5796' : '#ccc')};
+    min-height: 30px;
+    padding:2px 2px 0px 2px;
+  }
+
+  & select{
+    min-height: 30px;
+    padding:2px 2px 0px 2px;
+  }
+
+  & label{
+    min-height: 30px;
+    padding:2px 2px 0px 2px;
+    text-align:left;
+  }
+
+  & button{
+    width:50%;
+    padding:2px 2px 0px 2px;
+  }
+
+  @media (max-width: 600px){
+    width:100%;
+  }
+
+`;
 
 function AddTodo({setTodos}) {
     const inputRef = useRef();
     const selRef = useRef();
+    const [isValidText, setIsValidText] = useState(true)
 
     function handleAddTodo(event) {
         event.preventDefault();
         const text = event.target.elements.todoName.value
-        const done = event.target.elements.todoStatus.value
+        const done = event.target.elements.todoStatus.value === "false" ? false : true
+
+        if(text.trim().length === 0){
+          setIsValidText(false);
+          return;
+        }
         const todo = {
             id:'',
             text,
-            done
+            done:done
         }
         setTodos(prevTodos => {
             todo.id = prevTodos.length + 1;
             return prevTodos.concat(todo)
-        })
+        });
+        setIsValidText(true);
         inputRef.current.value = '';
         selRef.current.value = false;
     }
     
 
     return (
-      <div className={classes.AddTodoDiv}>
+      <AddTodoDiv invalid={!isValidText}>
         <form onSubmit={handleAddTodo}>
+          <label>Add New To Do</label>
           <input name="todoName" placeholder="Add New todo to your list" ref={inputRef} />
+          <label>Select the status of the toDo</label>
           <select name="todoStatus" ref={selRef}>
             <option value="false">Pending</option>
             <option value="true">Done</option>
           </select>
           <button type="submit">Add</button>
         </form>
-      </div>
+      </AddTodoDiv>
     );
   }
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,22 +1,48 @@
-import classes from './TodoList.module.css'
+import styled from 'styled-components'
+
+const TodoListDiv = styled.div`
+    background-color: #DED9E2;
+    width:60%;
+    justify-content: left;
+    height: 70vh;
+    
+    & table {
+        padding: 5px;
+        width: 100%;
+    }
+
+    & tr td {
+        padding:2px 5px 2px 5px;
+        list-style: none;
+        text-align: left;
+    }
+
+    & .tblHeader td{
+        font-weight: bold;
+    }
+
+    @media (max-width: 600px){
+        width:100%
+    }
+`;
 
 function TodoList(props) {
-    
+    console.log(props.todos) 
     const todoList = props.todos.map( todo => (
-        <tr key={todo.id}><td >{todo.text} </td><td> {todo.done ? "Done" : "Pending"}</td></tr>
+        <tr key={todo.id}><td >{todo.text} </td><td> { todo.done ? "Done" : <button>Mark as Done</button>}</td></tr>
     ));
     
     return (
-        <div className={classes.TodoListDiv}>
+        <TodoListDiv>
             <table>
                 <thead>
-                    <tr className={classes.tblHeader}><td>Task Name</td><td>Status</td></tr>
+                    <tr><td>Task Name</td><td>Status</td></tr>
                 </thead>
                 <tbody>
                     {todoList}
                 </tbody> 
             </table>
-        </div>
+        </TodoListDiv>
     );
 }
 
